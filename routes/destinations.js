bc372581fc05032a285817d985f1f6930439592d
Jeevitha <jeevitha.pugazh@gmail.com>
2025-04-11 13:45:17 -0400

    added view engine page details

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const activities = require("../data/activities");
 const destination = require("../data/destinations");
+const reviews = require("../data/reviews")
 
 router.get("/",(req,res)=>{
     const links = [
@@ -15,6 +16,21 @@ router.get("/",(req,res)=>{
       res.json({destination, links});
 })
 
+router.get("/:id",(req,res)=>{
+  const destination = destinations.find(d => d.id === parseInt(req.params.id));
+  const destinationActivities = activities.filter(a=>a.destinationId === destination.id);
+  const destinationReviews = reviews.filter(r => r.destinationId === destination.id);
 
+  if(!destination){
+    return res.status(404).send("Destination not found");
+  }
+
+  res.render("details",{
+    title: `${destination.name}`,
+    destination,
+    activities: destinationActivities,
+    reviews: destinationReviews,
+  });
+});
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const app = express();
 const PORT = 3000;
 const path = require("path");
 const error = require("./utilities/error");
-const destinations = require("./data/destinations")
+const destinations = require("./data/destinations");
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 
 // Routes
 const destinationRoutes = require("./routes/destinations");
@@ -25,53 +25,49 @@ app.use("/api/activities", activitieRoutes);
 app.use("/api/reviews", reviewsRoutes);
 
 app.get("/", (req, res) => {
-    res.render("home", { title: "Travel Guide", destinations});
-    
+  res.render("home", {
+    title: "Travel Guide",
+    destinations,
   });
+});
 
 //Adding HATEOAS links
 
-app.get("/api",(req,res)=>{
-    res.json({
-        links:[{
-            href : "/api",
-            rel: "api",
-            type: "GET"
-        },
-        {
-            href : "api/destinations",
-            rel: "destination",
-            type: "GET"
-        },
-        {
-            href : "api/activities",
-            rel: "activities",
-            type: "GET"
-        },
-        {
-            href : "api/reviews",
-            rel: "reviews",
-            type: "GET"
-        },
-    ]
-    })
-})
+app.get("/api", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "api/destinations",
+        rel: "destination",
+        type: "GET",
+      },
+      {
+        href: "api/activities",
+        rel: "activities",
+        type: "GET",
+      },
+      {
+        href: "api/reviews",
+        rel: "reviews",
+        type: "GET",
+      },
+    ],
+  });
+});
 
 //404 middleware
-app.use((req,res,next)=>{
-    next(error(404, "Resource Not found"));
-})
+app.use((req, res, next) => {
+  next(error(404, "Resource Not found"));
+});
 
 //error-handling middleware
-app.use((err,req,res,next)=>{
-    res.status(err.status || 500);
-    res.json({error:err.message});
-})
-
-
-
-
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(
+    `Server is running on http://localhost:${PORT}`
+  );
 });
